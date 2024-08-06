bdf3e51e2701d805e1bafcb82db6697ab4fd6b38
Cynthiadai <cynthiadai122@gmail.com>
2024-08-06 20:41:13 +1000

    install router

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+      <v-row>
+        <v-col>
+          <h1>About</h1>
+          <v-btn color="primary" @click="$router.push('/')">Go to Home</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AboutPage',
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
---
+++ src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+      <v-row>
+        <v-col>
+          <h1>Home</h1>
+          <v-btn color="primary" @click="$router.push('/about')">Go to About</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage',
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
